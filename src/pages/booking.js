e111afbef04b679b84a0ebcf510a84278c4e4f02
Tara <tara.byrne2001@gmail.com>
2023-03-09 19:59:24 +0000

    Update Error Message

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -260,19 +260,11 @@ function Booking(){
                     <>
                         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                                     <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                                         <h3 className="text-3xl font-semibold">
-                                            Date + Time is already taken
+                                            Date + Time is already taken. Please select another date/time.
                                         </h3>
-                                        <button
-                                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                            onClick={() => setShowError(false)}
-                                        >
-                                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                            </span>
-                                        </button>
                                     </div>
                                     <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                                         <button
@@ -280,7 +272,7 @@ function Booking(){
                                             type="button"
                                             onClick={() => setShowError(false)}
                                         >
-                                            Close
+                                            <strong>Close</strong>
                                         </button>
                                     </div>
                                 </div>
